1ceca82ab2961e6a52e254640fdaf2e15bbe36e0
g00345086 <g00345086@gmit.ie>
2022-03-09 10:03:09 +0000

    package and test report generated

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -1,4 +1,80 @@
 package ie.gmit;
 
 public class Employee {
+
+    String title;
+    String name;
+    int age;
+    String PPS;
+    long phone;
+    String EmploymentType;
+    public Employee(String title, String name, int age,String PPS,long phone,String employmentType) {
+        this.title = title;
+        if (name.length() > 3)
+        {
+            if (name.length()<25)
+            {
+                this.name = name;
+            }
+        }
+        if (PPS.length()==8)
+            this.PPS  = PPS;
+        int lgth = String.valueOf(phone).length();
+        if (lgth==9){
+            this.phone=phone;
+        }
+        this.EmploymentType=employmentType;
+        if (age > 15)
+            this.age = age;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPPS() {
+        return PPS;
+    }
+
+    public void setPPS(String PPS) {
+        this.PPS = PPS;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmploymentType() {
+        return EmploymentType;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        EmploymentType = employmentType;
+    }
+
 }
--- src/test/java/ie/gmit/TestEmploye.java
+++ src/test/java/ie/gmit/TestEmploye.java
@@ -1,4 +1,14 @@
 package ie.gmit;
 
+import org.junit.Test;
+
 public class TestEmploye {
+    @Test
+    public static void main(String args[]) {
+        /* Create two objects using constructor */
+        Employee employee1 = new Employee("Mr","Jone",17,"1234568f",
+                123456789,"Part-Time");
+
+    }
+
 }
